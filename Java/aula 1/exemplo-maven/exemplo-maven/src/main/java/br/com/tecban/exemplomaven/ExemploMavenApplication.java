07cd3f0c0433eec8bcc24107414be1a863f48a84
Gaazedo <gabrieltazevedo@gmail.com>
2021-07-05 15:31:24 -0300

    Subindo Aula 1 de Java

---
+++ Java/aula 1/exemplo-maven/exemplo-maven/src/main/java/br/com/tecban/exemplomaven/ExemploMavenApplication.java
@@ -0,0 +1,11 @@
+package br.com.tecban.exemplomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public  class ExemploMavenApplicationTests {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ Java/aula 1/exemplo-maven/exemplo-maven/src/test/java/br/com/tecban/exemplomaven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tecban.exemplomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
