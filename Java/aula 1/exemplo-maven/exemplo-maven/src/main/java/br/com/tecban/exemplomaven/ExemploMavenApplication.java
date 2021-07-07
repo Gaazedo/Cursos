fb75952e0cf36418fe0fc65d080e550ecf090bfc
Gaazedo <gabrieltazevedo@gmail.com>
2021-07-07 13:47:48 -0300

    Aula 1 atualizada Maven e Gradle

--- Java/aula 1/exemplo-maven/exemplo-maven/src/main/java/br/com/tecban/exemplomaven/ExemploMavenApplication.java
+++ Java/aula 1/exemplo-maven/exemplo-maven/src/main/java/br/com/tecban/exemplomaven/ExemploMavenApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public  class ExemploMavenApplicationTests {
+public class ExemploMavenApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ExemploMavenApplication.class, args);
     }
+
 }
